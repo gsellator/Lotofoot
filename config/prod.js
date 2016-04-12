6e9127a5506418d2cb437a0f0f440fb3226de920
gsellator <adrien@gsell.me>
2016-04-12 12:13:15 +0200

    analytics

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ export default {
   appFullName: "Lotofoot",
   description: "Euro 2016",
   apiUri: 'https://lotofoot-api-dev.herokuapp.com/api',
-//  trackingId: "UA-45826787-3",
+  trackingId: "UA-76304135-1",
 }
