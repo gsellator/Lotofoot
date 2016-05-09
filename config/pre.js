fa4d2901722f80186228ccb05cdc544c2576139c
gsellator <adrien@gsell.me>
2016-05-09 18:10:07 +0200

    prod

--- config/pre.js
+++ config/pre.js
@@ -2,7 +2,7 @@ export default {
   appName: "lotofoot",
   appFullName: "Lotofoot",
   description: "Pariez sur les matchs de l'Euro 2016",
-  appUri: 'http://localhost:3000',
+  appUri: 'http://lotofoot-pre.herokuapp.com',
   apiUri: 'https://lotofoot-api-dev.herokuapp.com/api',
-  trackingId: "UA-76304135-1",
+//  trackingId: "UA-76304135-1",
 }
