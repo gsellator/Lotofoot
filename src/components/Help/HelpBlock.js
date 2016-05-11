d2796372c3aa3bea72d63eb183377a8095bdd33f
gsellator <adrien@gsell.me>
2016-05-11 22:52:27 +0200

    help for first cnx

---
+++ src/components/Help/HelpBlock.js
@@ -0,0 +1,93 @@
+import React, { PropTypes, Component } from "react";
+//import { connectToStores } from "fluxible-addons-react";
+//import FormatDate from "../Helpers/FormatDate";
+//import Filters from "../Helpers/Filters";
+
+if (process.env.BROWSER) {
+  require("../../style/Help/HelpBlock.scss");
+}
+
+class HelpBlock extends Component {
+//  static contextTypes = {
+//    executeAction: PropTypes.func.isRequired,
+//    getStore: PropTypes.func.isRequired
+//  }
+
+  render() {
+    const { data, teamsData } = this.props;
+
+    return (
+      <div className="HelpBlock">
+        <div>
+          Amis Footix, le but de ce lotofoot est de pronostiquer le vainqueur et le score de chaque match de l'Euro.
+          Nous tenons compte du score final du match, prolongations incluses.
+          Dans le cas d'un match à élimination directe, si vous pronostiquez un match nul, vous devez aussi préciser quelle est
+          l'équipe que vous voyez victorieuse à l'issue des tirs aux buts.<br/><br/>
+
+          Le nombre de points que vos pronostics vous rapportent dépendent :<br/>
+          - Du stade de la compétition auquel appartient le match (match de poule, quart de finale...)<br/>
+          - De l'exactitude de votre pronostic (bon vainqueur, bon vainqueur et bonne différence de buts, bon vainqueur avec le score exact)<br/><br/>
+
+          Les points mis en jeu sont les suivants :
+        </div>
+
+        <div className="HelpTable">
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Bon vainqueur</th>
+                <th>Bonne diff de buts</th>
+                <th>Bon score</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Poules</th>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <th>1/8</th>
+                <td>2</td>
+                <td>4</td>
+                <td>6</td>
+              </tr>
+              <tr>
+                <th>1/4</th>
+                <td>3</td>
+                <td>5</td>
+                <td>8</td>
+              </tr>
+              <tr>
+                <th>1/2</th>
+                <td>4</td>
+                <td>6</td>
+                <td>10</td>
+              </tr>
+              <tr>
+                <th>Finale</th>
+                <td>5</td>
+                <td>7</td>
+                <td>12</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div>
+          Les pronostics peuvent être modifiés jusqu'au début du match. Bonne chance !
+        </div>
+      </div>
+    );
+  }
+}
+
+//HelpBlock = connectToStores(HelpBlock, ["HelpBlockStore"], (context) => {
+//  return {
+//    data: context.getStore("HelpBlockStore").getData(),
+//  };
+//}, {getStore: PropTypes.func});
+
+export default HelpBlock;
--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -1,6 +1,7 @@
 import React, { PropTypes, Component } from "react";
 import { connectToStores } from "fluxible-addons-react";
 import { NavLink, RouteStore } from "fluxible-router";
+import HelpBlock from "../components/Help/HelpBlock";
 
 if (process.env.BROWSER) {
   require("../style/Pages/HelpPage.scss");
@@ -20,68 +21,7 @@ class HelpPage extends Component {
              Règles du jeu
           </div>
           <div className="HelpPageCtnt">
-            <div>
-              Amis Footix, le but de ce lotofoot est de pronostiquer le vainqueur et le score de chaque match de l'Euro.
-              Nous tenons compte du score final du match, prolongations incluses.
-              Dans le cas d'un match à élimination directe, si vous pronostiquez un match nul, vous devez aussi préciser quelle est
-              l'équipe que vous voyez victorieuse à l'issue des tirs aux buts.<br/><br/>
-
-              Le nombre de points que vos pronostics vous rapportent dépendent :<br/>
-              - Du stade de la compétition auquel appartient le match (match de poule, quart de finale...)<br/>
-              - De l'exactitude de votre pronostic (bon vainqueur, bon vainqueur et bonne différence de buts, bon vainqueur avec le score exact)<br/><br/>
-
-              Les points mis en jeu sont les suivants :
-            </div>
-
-            <div className="HelpTable">
-              <table>
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th>Bon vainqueur</th>
-                    <th>Bonne diff de buts</th>
-                    <th>Bon score</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th>Poules</th>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                  </tr>
-                  <tr>
-                    <th>1/8</th>
-                    <td>2</td>
-                    <td>4</td>
-                    <td>6</td>
-                  </tr>
-                  <tr>
-                    <th>1/4</th>
-                    <td>3</td>
-                    <td>5</td>
-                    <td>8</td>
-                  </tr>
-                  <tr>
-                    <th>1/2</th>
-                    <td>4</td>
-                    <td>6</td>
-                    <td>10</td>
-                  </tr>
-                  <tr>
-                    <th>Finale</th>
-                    <td>5</td>
-                    <td>7</td>
-                    <td>12</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-
-            <div>
-              Les pronostics peuvent être modifiés jusqu'au début du match. Bonne chance !
-            </div>
-
+            <HelpBlock />
             <div className="Footer">
               En cas de problème ou pour toute question, contactez-nous à l'adresse <a href="mailto:hello@lotofoot.radio97.fr">hello@lotofoot.radio97.fr</a>.
             </div>
