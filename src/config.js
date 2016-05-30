6ec4349d004f7c6aadde6707d4310d9e1a887d8e
gsellator <adrien@gsell.me>
2016-05-30 08:46:58 +0200

    analytics

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
   appName: process.env.APP_NAME || 'lotofoot-dev',
   apiUri: process.env.API_URI || 'https://lotofoot-api-dev.herokuapp.com/api',
-  trackingId: process.env.TRACKING_ID || undefined,
+//  trackingId: process.env.TRACKING_ID || undefined,
+  trackingId: process.env.TRACKING_ID || 'UA-45826787-6',
 }
