b16c6b02a301f4b34ba2df641d166c9bf0bbe08a
Adrien Gsell <adrien@gsell.me>
2018-07-02 13:06:29 +0200

    fix

--- src/config/dev.js
+++ src/config/dev.js
@@ -8,7 +8,7 @@ export default {
   language: 'en',
 
   //apiUri: 'http://localhost:5000/api',
-  //apiUri: 'https://lotofoot-api-dev.herokuapp.com/api',
-  apiUri: 'https://lotofoot-api.herokuapp.com/api',
+  apiUri: 'https://lotofoot-api-dev.herokuapp.com/api',
+//  apiUri: 'https://lotofoot-api.herokuapp.com/api',
   appName: 'lotofoot-dev',
 };
